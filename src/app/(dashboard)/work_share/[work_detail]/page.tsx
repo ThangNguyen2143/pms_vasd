977158cd77d115aa6a0f87b5300e5dc0ada1cc86
ThangNguyen2143 <125419960+ThangNguyen2143@users.noreply.github.com>
2025-07-30 08:33:39 +0700

    fix build

--- src/app/(dashboard)/work_share/[work_detail]/page.tsx
+++ src/app/(dashboard)/work_share/[work_detail]/page.tsx
@@ -1,16 +1,13 @@
-import { decodeBase64, encodeBase64 } from "~/lib/services";
+import { decodeBase64 } from "~/lib/services";
 import RequirementDetailClient from "../../requirement/[requirement]/requirment-detail-client";
 import { fetchData } from "~/lib/api-client";
-import { ProjectLocation, RequirementType } from "~/lib/types";
-import { toast } from "sonner";
+import { RequirementType } from "~/lib/types";
 
 async function WorkDetailPage(prop: {
   params: Promise<{ work_detail: string }>;
 }) {
   const { work_detail } = await prop.params;
-  const { requirement_id, project_id } = decodeBase64(
-    decodeURIComponent(work_detail)
-  ) as {
+  const { requirement_id } = decodeBase64(decodeURIComponent(work_detail)) as {
     requirement_id: number;
     project_id: number;
   };
@@ -20,26 +17,11 @@ async function WorkDetailPage(prop: {
     endpoint: endpointTypeRequired,
     cache: "default",
   });
-  const locationList = await fetchData<ProjectLocation[]>({
-    endpoint: "/project/location/" + encodeBase64({ project_id }),
-  });
-  if (locationList.code == 404) {
-    locationList.value = [];
-  }
-  if (locationList.code != 200 && locationList.code != 404) {
-    toast.error(locationList.message);
-    locationList.value = [];
-  }
-  let location = locationList.value;
-  if (locationList.code == 200) {
-    location = locationList.value.map((loca) => ({ ...loca, project_id }));
-  }
+
   return (
     <RequirementDetailClient
       requirement_id={requirement_id}
       typeList={requireTypeList.value}
-      project_id={project_id}
-      locations={location}
     />
   );
 }
